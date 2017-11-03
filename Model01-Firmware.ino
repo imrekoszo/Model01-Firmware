988814beab3b3c2f022e182deb4636cf8e5c3559
Imre Kószó <imrekoszo@users.noreply.github.com>
2017-11-03 08:49:52 +0000

    swap a few symbol/HUN letter keys around

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -132,11 +132,11 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_LeftGui, Key_Backspace, Key_LeftControl, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   Key_Minus,     Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Equals,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_LeftBracket,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightBracket,
-   Key_RightShift, Key_RightControl, Key_Spacebar, Key_LeftAlt,
+   Key_LeftBracket, Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_RightBracket,
+   Key_Enter,       Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                    Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_RightAlt,    Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_RightShift,  Key_RightControl, Key_Spacebar, Key_LeftAlt,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
